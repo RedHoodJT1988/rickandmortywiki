b0f3329ab9afc30c267c884c0ffd159aefc7708f
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2021-12-06 08:16:20 -0600

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Search from "./Search/Search";
+import Card from "./Card/Card";
+import Pagination from "./Pagination/Pagination";
+import Filter from "./Filter/Filter";
+import Navbar from "./Navbar/Navbar";
+
+export { Search, Card, Pagination, Filter, Navbar };
